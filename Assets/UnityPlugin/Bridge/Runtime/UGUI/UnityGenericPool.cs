1016a741494d84b71bf21742e63bd4ad47e98fd8
吴琼 <bbstudio@qq.com>
2026-07-12 17:07:02 +0800

    feat: UnityGenericPool

---
+++ Assets/UnityPlugin/Bridge/Runtime/UGUI/UnityGenericPool.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_2021_3_OR_NEWER
+using UnityEngine.Pool;
+#else
+using UnityEngine.UI;
+#endif
+
+namespace UnityPlugin.Bridge
+{
+    public class UnityGenericPool<T> where T : class, new()
+    {
+#if UNITY_2021_3_OR_NEWER
+        public static T Get()
+        {
+            return GenericPool<T>.Get();
+        }
+
+        public static void Release(T toRelease)
+        {
+            GenericPool<T>.Release(toRelease);
+        }
+#else
+        internal static readonly ObjectPool<T> s_Pool = new ObjectPool<T>(null, null);
+
+        public static T Get()
+        {
+            return s_Pool.Get();
+        }
+
+        public static void Release(T toRelease)
+        {
+            s_Pool.Release(toRelease);
+        }
+#endif
+    }
+}
