a93a0ff8fe9ea2590871db6a5410626e7b2e04ce
吴琼 <bbstudio@qq.com>
2026-07-25 20:46:18 +0800

    feat: 解耦 UnityEngine.UI

---
+++ Assets/UnityPlugin/Bridge/Runtime/UnityGenericPool.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_2021_3_OR_NEWER
+using UnityEngine.Pool;
+#endif
+
+namespace UnityPlugin.Bridge
+{
+    public class UnityGenericPool<T> where T : class, new()
+    {
+#if UNITY_2021_3_OR_NEWER
+
+        public static T Get() => GenericPool<T>.Get();
+        public static void Release(T toRelease)=>  GenericPool<T>.Release(toRelease);
+
+#else
+
+        static readonly UnityObjectPool<T> s_Pool = new UnityObjectPool<T>(null, null);
+
+        public static T Get() => s_Pool.Get();
+        public static void Release(T toRelease) => s_Pool.Release(toRelease);
+
+#endif
+    }
+}
---
+++ Assets/UnityPlugin/Bridge/Runtime/UnityListPool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+#if UNITY_2021_3_OR_NEWER
+using UnityEngine.Pool;
+#endif
+
+namespace UnityPlugin.Bridge
+{
+    public static class UnityListPool<T>
+    {
+#if UNITY_2021_3_OR_NEWER
+
+        public static List<T> Get() => ListPool<T>.Get();
+        public static void Release(List<T> toRelease) => ListPool<T>.Release(toRelease);
+
+#else
+
+        static readonly UnityObjectPool<List<T>> s_ListPool = new UnityObjectPool<List<T>>(null, Clear);
+        static void Clear(List<T> l) { l.Clear(); }
+
+        public static List<T> Get() => s_ListPool.Get();
+        public static void Release(List<T> toRelease) => s_ListPool.Release(toRelease);
+
+#endif
+    }
+}
